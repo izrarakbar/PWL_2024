be17fa3ff134436c770f4ef502964ce4e65a7141
Izrar <izar2653@gmail.com>
2024-02-28 08:04:26 +0700

    upload

---
+++ app/Http/Controllers/FoodBeverageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FoodBeverageController extends Controller
+{
+    public function foodBeverage()
+    {
+
+        return view('foodBeverage');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+
+// use App\Http\Controllers\PageController;
+// use App\Http\Controllers\PhotoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,86 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeCareController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\BeautyHealthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BabyController;
+use App\Http\Controllers\FoodBeverageController;
+
+Route::get('/food', [FoodBeverageController::class, 'foodBeverage']);
+
+Route::get('/beauty', [BeautyHealthController::class, 'BeautyHealth']);
+
+Route::get('/baby', [BabyController::class, 'babyKid']);
+
+Route::get('/homecare', [HomeCareController::class, 'homeCare']);
+
+Route::get('/home', [HomeController::class, 'home']);
+
+Route::get('/penjualan', [PenjualanController::class, 'penjualan']);
+
+
+use App\Http\Controllers\UserController;
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
+
+
+
+
+
+// Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'Izrar']);
+//     });
+
+
+
+// Route::resource('photos', PhotoController::class);
+
+// Route::get('/hello', [WelcomeController::class,'hello']);
+
+// Route::get('/', [PageController::class, 'index']);
+
+// Route::get('/about', [PageController::class, 'about']);
+
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Route::group(['prefix' => 'batch'], function () {
+
+
+// Route::get('/world', function () {
+//     return 'World';
+// });
+
+// Route::get('/izr/{name?}', function ($name=' IZRAR AKBAR MUNIRZA') {
+//     return 'Nama Saya'.$name;
+// });
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+//     });
+
+// Route::get('/world', function () {
+//     return 'World';
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+// });
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+//     });
+// Route::get('/user/profile', function () {
+//     $name = 'Izar'; 
+//     return "Nama Saya $name";
+// })->name('profile');
+
+// Route::get('/userprofile', function() {
+//     return redirect()->route('profile'); 
+// });
+
+// });
